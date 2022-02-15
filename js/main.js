7e7430fa5c52d03ac250f5e06fdecd0c24c884ed
Ahmedbash98 <W0777010@myscc.ca>
2022-02-14 21:30:18 -0500

    updated the main.js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
 // WEB303 Assignment 2
+// Ahmed Abdella 
+//0777010
+
+
+$(function () {
+	
+	let xhr = new XMLHttpRequest;
+	let content = document.getElementById('content');
+
+	$('#prospect').click(function () {
+		
+		xhr.open('GET', 'prospect.html', true);
+
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			content.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send();
+
+		$("div#content").hide();
+		$("div#content").slideDown("slow");
+		
+	});
+
+	$('#convert').click(function () {
+		
+		xhr.open('GET', 'convert.html', true);
+	
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			content.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send();
+
+		$("div#content").hide();
+		$("div#content").slideDown("slow");
+		
+	});
+
+	$('#retain').click(function () {
+		
+		xhr.open('GET', 'retain.html', true);
+
+		xhr.onload = function () {
+		if ( xhr.status === 200 ){
+			content.innerHTML = xhr.responseText;
+			}
+		}
+		xhr.send();
+
+		$("div#content").hide();
+		$("div#content").slideDown("slow");
+		
+	});
+
+});
+
+
+
